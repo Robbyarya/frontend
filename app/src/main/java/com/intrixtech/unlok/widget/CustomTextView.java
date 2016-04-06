692855bd86120ef6a56713db8c199f3158d084c4
Prateek Arora <prateekksw@gmail.com>
2016-04-06 21:28:21 +0530

    added project setup

---
+++ app/src/main/java/com/intrixtech/unlok/widget/CustomTextView.java
@@ -0,0 +1,38 @@
+package com.intrixtech.unlok.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.intrixtech.unlok.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Arti Verma
+ * To change this template use File | File Templates.
+ */
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+        String fontName = styledAttrs.getString(R.styleable.CustomTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+        this.setPaintFlags(this.getPaintFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+    }
+
+}
